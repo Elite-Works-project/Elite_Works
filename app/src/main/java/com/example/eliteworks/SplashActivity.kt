11c6a5930cf8f382e9cc1b18688d9e8d7302ecb2
Bhut Tushar <bhuttushar2019@gmail.com>
2023-05-19 12:35:59 +0530

    Initial Commit

---
+++ app/src/main/java/com/example/eliteworks/SplashActivity.kt
@@ -0,0 +1,23 @@
+package com.example.eliteworks
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+
+@SuppressLint("CustomSplashScreen")
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        supportActionBar?.hide()
+
+        Handler(Looper.getMainLooper()).postDelayed(
+            Runnable {
+                startActivity(Intent(this,LoginActivity::class.java))
+            },2500)
+    }
+}
