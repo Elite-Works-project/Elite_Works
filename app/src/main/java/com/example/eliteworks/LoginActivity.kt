764955cd853645cf702a9226b1853617a35eb76a
Bhut Tushar <bhuttushar2019@gmail.com>
2023-05-19 19:43:25 +0530

    created login,signup activities Pending : create login, signup backend

--- app/src/main/java/com/example/eliteworks/LoginActivity.kt
+++ app/src/main/java/com/example/eliteworks/LoginActivity.kt
@@ -1,13 +1,21 @@
 package com.example.eliteworks
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.eliteworks.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityLoginBinding
     override fun onCreate(savedInstanceState: Bundle?) {
+        binding = ActivityLoginBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        setContentView(binding.root)
 
         supportActionBar?.hide()
+
+        binding.btnSignupFromLogin.setOnClickListener{
+            startActivity(Intent(this,SignUpActivity::class.java))
+        }
     }
 }
--- app/src/main/java/com/example/eliteworks/SignUpActivity.kt
+++ app/src/main/java/com/example/eliteworks/SignUpActivity.kt
@@ -1,13 +1,21 @@
 package com.example.eliteworks
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.eliteworks.databinding.ActivitySignUpBinding
 
 class SignUpActivity : AppCompatActivity() {
+    private lateinit var binding: ActivitySignUpBinding
     override fun onCreate(savedInstanceState: Bundle?) {
+        binding = ActivitySignUpBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_sign_up)
+        setContentView(binding.root)
 
         supportActionBar?.hide()
+
+        binding.btnLoginFromSignup.setOnClickListener {
+            onBackPressed()
+        }
     }
 }
